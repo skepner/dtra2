7401ae13400c571e5e78793a084bbafb4d55e9e8
Eugene Skepner <github@skepner.eu>
2019-01-08 11:35:32 +0100

    xlnt.hh

---
+++ cc/xlnt.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#pragma GCC diagnostic ignored "-Wnewline-eof"
+#endif
+#include "xlnt/xlnt.hpp"
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
